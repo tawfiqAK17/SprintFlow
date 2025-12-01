73961cccf3e8c79bb98267e7b461ab3fbc6e704e
tawfiq akdir <tawfiqakdir@gmail.com>
2025-12-01 14:55:25 +0100

    initial commit

---
+++ src/main/java/com/ensa/SpdrintFlow/SpdrintFlowApplication.java
@@ -0,0 +1,13 @@
+package com.ensa.SpdrintFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpdrintFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpdrintFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ensa/SpdrintFlow/SpdrintFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensa.SpdrintFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpdrintFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
