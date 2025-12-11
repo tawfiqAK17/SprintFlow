c83480a1ac94ffda0a51f3e977942d93bef84170
tawfiq akdir <tawfiqakdir@gmail.com>
2025-12-11 15:20:28 +0100

    removing the AuthenticationManager bean because it's not needed if using the jwt authentication

--- src/main/java/com/ensa/SprintFlow/config/SecurityConfig.java
+++ src/main/java/com/ensa/SprintFlow/config/SecurityConfig.java
@@ -42,11 +42,6 @@ public class SecurityConfig {
   }
 
   @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) {
-    return config.getAuthenticationManager();
-  }
-
-  @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
